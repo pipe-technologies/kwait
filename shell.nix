4818a1a323916e3c56df94436f570d99c7b0f1ad
Isaac Pedisich <isaac@pipe.com>
2024-04-05 10:43:35 -0700

    Add python/poetry/just to shell.nix
    Tell me you're not using nix without telling me you're not using nix :)

--- shell.nix
+++ shell.nix
@@ -2,7 +2,15 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.hello
+    # "Just" a command runner: https://github.com/casey/just
+    pkgs.just
+
+    pkgs.poetry
+    # Install various versions of python so we can test across them
+    pkgs.python39
+    pkgs.python310
+    pkgs.python311
+    pkgs.python312
 
     # keep this line if you use bash
     pkgs.bashInteractive
